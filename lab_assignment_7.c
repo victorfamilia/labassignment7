8b1ac14e870c994eea05edcf95c3ff69a5d5829e
Victor Familia <vicmfam@gmail.com>
2023-03-22 19:53:51 -0400

    completed lab

--- lab_assignment_7.c
+++ lab_assignment_7.c
@@ -1,4 +1,10 @@
-#include <stdio.h>
+#include <string.h> 
+#include <stdio.h> 
+#include <stdlib.h> 
+
+//for each iteration, count the number of swaps for that iteration,
+//use malloc, declare array with the appropriate size, add numbers
+//to it, call bubble sort function.
  
 void swap(int* xp, int* yp)
 {
@@ -7,19 +13,23 @@ void swap(int* xp, int* yp)
     *yp = temp;
 }
  
-// A function to implement bubble sort
+//function to implement bubble sort
 void bubbleSort(int arr[], int n)
 {
-    int i, j;
-    for (i = 0; i < n - 1; i++)
- 
-        // Last i elements are already in place
-        for (j = 0; j < n - i - 1; j++)
-            if (arr[j] > arr[j + 1])
-                swap(&arr[j], &arr[j + 1]);
+  int i, j, numSwap;
+  for (i = 0; i < n - 1; i++){
+    numSwap = 0;
+    for (j = 0; j < n - i - 1; j++){
+      if (arr[j] > arr[j + 1]){
+        swap(&arr[j], &arr[j + 1]);
+        numSwap++;
+      }
+    }
+    printf("pass #%d: %d\n", i+1, numSwap);
+  }
 }
- 
-/* Function to print an array */
+
+//function to prints array
 void printArray(int arr[], int size)
 {
     int i;
@@ -27,14 +37,12 @@ void printArray(int arr[], int size)
         printf("%d ", arr[i]);
     printf("\n");
 }
- 
-// Driver program to test above functions
+
+//tests above functions
 int main()
 {
     int arr[] = { 97, 16, 45, 63, 13, 22, 7, 58, 72 };
     int n = sizeof(arr) / sizeof(arr[0]);
     bubbleSort(arr, n);
-    printf("Sorted array: \n");
-    printArray(arr, n);
     return 0;
 }
