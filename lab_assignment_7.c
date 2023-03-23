1a92cc6055eb018b3c84b2a804d777db9f1647b6
Victor Familia <vicmfam@gmail.com>
2023-03-22 23:51:41 -0400

    lab completed

--- lab_assignment_7.c
+++ lab_assignment_7.c
@@ -17,10 +17,13 @@ void swap(int* xp, int* yp)
 void bubbleSort(int arr[], int n)
 {
   int i, j, numSwap;
-  for (i = 0; i < n - 1; i++){
+  for (i = 0; i < n - 1; i++)
+  {
     numSwap = 0;
-    for (j = 0; j < n - i - 1; j++){
-      if (arr[j] > arr[j + 1]){
+    for (j = 0; j < n - i - 1; j++)
+    {
+      if (arr[j] > arr[j + 1])
+      {
         swap(&arr[j], &arr[j + 1]);
         numSwap++;
       }
